fa3ebd1f4e628195dcc0a429fe9d40180a78c42a
isri12 <ikinfu@terpmail.umd.edu>
2023-04-14 14:44:43 -0400

    Add files via upload

---
+++ terraform_aws/main.tf
@@ -0,0 +1,31 @@
+terraform{
+    required_providers{
+        aws = {
+            source = "hashicorp/aws"
+            version = "~> 4.16"
+        }
+    }
+
+required_version = ">= 1.2.0"
+
+}
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "s3_bucket_israel" {
+    bucket = "tcb-app-qa-ik" #unique name 
+  
+}
+
+#block public access
+resource "aws_s3_bucket_public_access_block" "s3_block_israel" {
+    bucket = aws_s3_bucket.s3_bucket_israel.id
+
+    block_public_acls = true
+    block_public_policy = true
+    ignore_public_acls = true
+    restrict_public_buckets = true
+    
+}
